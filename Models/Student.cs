2d0a657d77344d2b0185829a293af7280f4a89be
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-10-10 19:47:54 +0300

    Change model id type to long

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,7 +2,7 @@ namespace PostgresTest.Models
 {
   public class Student
   {
-    public int    Id    { get; set; }
+    public long   Id    { get; set; }
     public string Name  { get; set; }
     public string Email { get; set; }
 
