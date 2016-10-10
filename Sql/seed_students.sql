4a6745f12ae1dfb1827a3385d5959fa135f6165e
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-10-10 19:41:57 +0300

    Add one more person to seed list

--- Sql/seed_students.sql
+++ Sql/seed_students.sql
@@ -1,5 +1,6 @@
-INSERT INTO students VALUES
+INSERT INTO students (name, email) VALUES
   ('John Doe',     'john.doe@example.com'),
   ('John Shepard', 'john.shepard@example.com'),
-  ('John Snow',    'john.snow@example.com')
+  ('John Snow',    'john.snow@example.com'),
+  ('Ned Stark',    'ned.stark@example.com')
 ;
